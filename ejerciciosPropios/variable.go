c5918508795c1936d963e89f16ec596b72aae2ef
GabrielBlanc0o <gtbvacation@outlook.es>
2026-04-30 11:53:32 -0500

    add initial implementation of variable declarations and print statements in Go

---
+++ ejerciciosPropios/variable.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+
+	//var nameOne string = "Hola"
+	//var nameTwo = "Pepas"
+	//var nameThree string
+
+	//fmt.Println(nameOne, nameTwo, nameThree)
+
+	//nameOne = "peach"
+	//nameThree = "pepitas"
+
+	//fmt.Println(nameOne, nameTwo, nameThree)
+
+	//na/meFour := "yoshi"
+	//fmt.Println(nameFour)
+
+	// ints
+
+	var ageOne int = 20
+	var ageTwo = 30.1
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits y memoria
+//	var numeroUno int8 = 25
+	//var numeroDos int8 = -128
+//	var numeroTres uint16 =  256    // no negativo
+
+	var valorUno
+
+}
